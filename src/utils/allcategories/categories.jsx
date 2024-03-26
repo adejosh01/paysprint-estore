76d13495ccfcf07b6ae13a231b219936bd23a04a
Praise Ebuka <praise.njoga@gmail.com>
2024-03-26 14:35:23 +0100

    .

--- src/utils/allcategories/categories.jsx
+++ src/utils/allcategories/categories.jsx
@@ -1,37 +1,31 @@
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 
-const CategoriesData = ({title}) => {
-  const [categories, setData] = useState([]);
+const CategoriesData = ({ title }) => {
+  const [categories, setCategories] = useState([]);
+  const [loading, setLoading] = useState(true);
   const apiUrl = process.env.REACT_APP_API_URL || 'https://paysprint.ca/api/v1';
 
   useEffect(() => {
     document.title = title;
     window.scrollTo(0, 0);
 
-    axios.get(`${apiUrl}/ashopree/product/category`) 
-    .then(response => {
-      setData(response.data.data);
-    })
-
-    .catch(error => {
-      console.error('Error fetching data:', error);
-    });
-
+    axios.get(`${apiUrl}/ashopree/product/category`)
+      .then(response => {
+        setCategories(response.data.data);
+        setLoading(false);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+        setLoading(false);
+      });
   }, [apiUrl, title]);
 
-  const totalCategories = categories.length;
-  const msg = "No categories data available.";
-
-
-  if (Array.isArray(categories)) {
-    return totalCategories;
-
-  } else {
-    return msg;
+  if (loading) {
+    return <p style={{ textAlign: 'center', fontSize: '2rem' }}>Loading......</p>;
   }
-  
+
+  return categories.length !== 0 ? categories : <p>No categories data available.</p>;
 };
 
 export default CategoriesData;
-
