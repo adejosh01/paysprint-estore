29eef6c37a59340065bdc8f614d9f2975f9f7690
Praise Ebuka <praise.njoga@gmail.com>
2024-04-13 17:02:48 +0100

    responsive map view

--- src/pages/contact/contact.jsx
+++ src/pages/contact/contact.jsx
@@ -45,7 +45,7 @@ export const Contact = ({ title }) => {
                   <div className="b4Title">
                     <div className="title">
                       <h2> Contact Us </h2>
-                      <p> Check out products or services from our registered stores </p>
+                      <p> Feel free to reach us anytime, any day, from anywhere!! </p>
                     </div>
                   
                     <button type="button" className="home-button" onClick={ () => handleClick('/', navigate)}>
@@ -56,12 +56,12 @@ export const Contact = ({ title }) => {
         </section>
 
         <section className="map">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2887.3161285371693!2d-79.37994522334242!3d43.64159075318092!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d4cb2baa813d5f%3A0xf7c39220b5bad1e!2sWaterpark%20Place!5e0!3m2!1sen!2sid!4v1701869472878!5m2!1sen!2sid" title="20 Bay St., Toronto, ON M5J 2N8, Canada" width="1300" height="600" frameborder="0" style={{ border: '0' }} allowfullscreen="">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2887.3161285371693!2d-79.37994522334242!3d43.64159075318092!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89d4cb2baa813d5f%3A0xf7c39220b5bad1e!2sWaterpark%20Place!5e0!3m2!1sen!2sid!4v1701869472878!5m2!1sen!2sid" title="20 Bay St., Toronto, ON M5J 2N8, Canada" width="100%" height="600" frameborder="0" style={{ border: '0' }} allowfullscreen="">
           </iframe>
         </section>
 
         <section className="form">
-          
+
         </section>
 
     </div>
