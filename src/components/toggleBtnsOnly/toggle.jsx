b1aa62740803bf956dfb6953616cd81417f2e0c8
Praise Ebuka <praise.njoga@gmail.com>
2024-04-22 13:31:43 +0100

    error trying to fix the *ALL* toggle

--- src/components/toggleBtnsOnly/toggle.jsx
+++ src/components/toggleBtnsOnly/toggle.jsx
@@ -2,10 +2,10 @@ import React, {useState } from 'react';
 import { Space, Switch } from 'antd';
 
 
-const Toggle = () => {
+const Toggle = ({ checked, onChange}) => {
   // The false param in the following useState is for the setting the toglling
   // to Off.
-  const [input, setInput] = useState(false);
+  // const [input, setInput] = useState(false);
   
   // Just a litle testing here to check the togglling method
   // if (input === true) {
@@ -16,19 +16,43 @@ const Toggle = () => {
   //   console.log("I don't even know what I want");
   // }
 
+  const ToggleGroup = () => {
+    const [allChecked, setAllChecked] = useState(false);
+  
+    const handleAllToggle = () => {
+      setAllChecked(!allChecked);
+    };
+  
+    const handleSingleToggle = () => {
+      // Logic to handle individual toggle
+    };
+
   return (
-    <Space direction="vertical" >
+    // <Space direction="vertical" >
+    //   <Space wrap>
+    //     <Switch checked={input} 
+    //       style={{ backgroundColor: input ? '#4F0B92' : '#d2d2d2', borderColor: input ? '#4F0B92' : '#d2d2d2' }}
+    //       checkedChildren={<span style={{ color: '#fff' }}> On </span>}
+    //       unCheckedChildren={<span style={{ color: '#4F0B92' }}> Off </span>}
+    //       onChange={() => {
+    //         setInput(!input);
+    //       }} />
+    //   </Space>
+    // </Space>
+
+    <Space direction="vertical">
       <Space wrap>
-        <Switch checked={input} 
-          style={{ backgroundColor: input ? '#4F0B92' : '#d2d2d2', borderColor: input ? '#4F0B92' : '#d2d2d2' }}
+        <Switch
+          checked={checked}
+          onChange={onChange}
           checkedChildren={<span style={{ color: '#fff' }}> On </span>}
           unCheckedChildren={<span style={{ color: '#4F0B92' }}> Off </span>}
-          onChange={() => {
-            setInput(!input);
-          }} />
+        />
       </Space>
     </Space>
   );
+
+  
 
 };
 
