26fe4f448b5e0ad34fe5f39d21f52408bd0fe7c7
Praise Ebuka <praise.njoga@gmail.com>
2024-04-22 13:50:06 +0100

    the toggle on and off is now working as supposed

--- src/components/toggleBtnsOnly/toggle.jsx
+++ src/components/toggleBtnsOnly/toggle.jsx
@@ -1,44 +1,19 @@
-import React, {useState } from 'react';
+import React from 'react';
 import { Space, Switch } from 'antd';
 
 
 const Toggle = ({ checked, onChange}) => {
-  // The false param in the following useState is for the setting the toglling
-  // to Off.
-  // const [input, setInput] = useState(false);
-  
+
   // Just a litle testing here to check the togglling method
-  // if (input === true) {
+  // if (checked === true) {
   //   console.log("The option for this is now ON");
-  // } else if (input === false) {
+  // } else if (checked === false) {
   //   console.log("The option for this is now OFF");
   // } else {
   //   console.log("I don't even know what I want");
   // }
 
-  const ToggleGroup = () => {
-    const [allChecked, setAllChecked] = useState(false);
-  
-    const handleAllToggle = () => {
-      setAllChecked(!allChecked);
-    };
-  
-    const handleSingleToggle = () => {
-      // Logic to handle individual toggle
-    };
-
   return (
-    // <Space direction="vertical" >
-    //   <Space wrap>
-    //     <Switch checked={input} 
-    //       style={{ backgroundColor: input ? '#4F0B92' : '#d2d2d2', borderColor: input ? '#4F0B92' : '#d2d2d2' }}
-    //       checkedChildren={<span style={{ color: '#fff' }}> On </span>}
-    //       unCheckedChildren={<span style={{ color: '#4F0B92' }}> Off </span>}
-    //       onChange={() => {
-    //         setInput(!input);
-    //       }} />
-    //   </Space>
-    // </Space>
 
     <Space direction="vertical">
       <Space wrap>
--- src/pages/community/community.jsx
+++ src/pages/community/community.jsx
@@ -18,6 +18,27 @@ export const Community = ({ title }) => {
     setIsOpen(!isOpen);
   };
 
+  const [allChecked, setAllChecked] = useState(false);
+  const [individualToggles, setIndividualToggles] = useState({ productsListed: false, storeAvailable: false, questionSubmitted: false, answerSubmitted: false, productPrice: false, priceReduction: false, partner: false, tags: false, });
+
+  const handleAllToggle = () => {
+    setAllChecked(!allChecked);
+    setIndividualToggles(prevState => {
+      const newState = {};
+      for (const key in prevState) {
+        newState[key] = !allChecked;
+      }
+      return newState;
+    });
+  };
+
+  const handleSingleToggle = (toggleName) => {
+    setIndividualToggles(prevState => ({
+      ...prevState,
+      [toggleName]: !prevState[toggleName],
+    }));
+  };
+
   useEffect(() => {
     document.title = title;
     window.scrollTo(0, 0);
@@ -57,15 +78,15 @@ export const Community = ({ title }) => {
                         {isOpen && (
                             <div className="settingsDetails">
                                 <div className="all-categories">
-                                    <p> New Products Listed <Toggle /> </p>
-                                    <p> New Store available <Toggle /> </p> 
-                                    <p> New Question submitted <Toggle /> </p>
-                                    <p> New Answer submitted <Toggle /> </p>
-                                    <p> New Product price <Toggle /> </p>
-                                    <p> Price Reduction <Toggle /> </p>
-                                    <p> New Partner <Toggle /> </p>
-                                    <p> New Tags <Toggle /> </p>
-                                    <p> All <Toggle /> </p>
+                                    <p> New Products Listed <Toggle checked={individualToggles.productsListed} onChange={() => handleSingleToggle('productsListed')} /> </p>
+                                    <p> New Store available <Toggle checked={individualToggles.storeAvailable} onChange={() => handleSingleToggle('storeAvailable')} /> </p> 
+                                    <p> New Question submitted <Toggle checked={individualToggles.questionSubmitted} onChange={() => handleSingleToggle('questionSubmitted')} /> </p>
+                                    <p> New Answer submitted <Toggle checked={individualToggles.answerSubmitted} onChange={() => handleSingleToggle('answerSubmitted')} /> </p>
+                                    <p> New Product price <Toggle checked={individualToggles.productPrice} onChange={() => handleSingleToggle('productPrice')} /> </p>
+                                    <p> Price Reduction <Toggle checked={individualToggles.priceReduction} onChange={() => handleSingleToggle('priceReduction')} /> </p>
+                                    <p> New Partner <Toggle checked={individualToggles.partner} onChange={() => handleSingleToggle('partner')} /> </p>
+                                    <p> New Tags <Toggle checked={individualToggles.tags} onChange={() => handleSingleToggle('tags')} /> </p>
+                                    <p> All <Toggle checked={allChecked} onChange={handleAllToggle} /> </p>
                                 </div>
                             </div>
                         )}
