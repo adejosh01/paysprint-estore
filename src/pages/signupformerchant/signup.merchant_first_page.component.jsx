6edbb5a1f6c37560449484f974374bcd8cd64642
Praise Ebuka <praise.njoga@gmail.com>
2024-04-26 16:36:08 +0100

    Everything about merchant registration added...remaining few edits pt-2

---
+++ src/pages/signupformerchant/signup.merchant_first_page.component.jsx
@@ -0,0 +1,138 @@
+import { SignupFormContext } from "context/signup-form.context";
+
+import "react-datepicker/dist/react-datepicker.css";
+import { useContext } from "react";
+import {
+  formatDateToInputDate,
+  formatInputDateToDate,
+  getMaxDOBForAdult,
+} from "utils/utils";
+
+const MAX_DOB_FOR_ADULT = getMaxDOBForAdult();
+
+export const SignupMerchantFirstPage = () => {
+  const {
+    address,
+    city,
+    country,
+    dateOfBirth,
+    firstName,
+    lastName,
+    postalCode,
+    state,
+    phone,
+    setAddress,
+    setCity,
+    setCountry,
+    setDateOfBirth,
+    setFirstName,
+    setLastName,
+    setPostalCode,
+    setState,
+    setPhone,
+  } = useContext(SignupFormContext);
+
+  return (
+    <div className="signup-second-page">
+      <h1>Create Your Account</h1>
+      <em>To create your account please provide your details</em>
+      
+      <form>
+        <div className="names">
+          <label htmlFor="first-name">
+            First name<span>*</span>
+            <input
+              type="text"
+              placeholder="First name"
+              id="first-name"
+              onChange={(e) => setFirstName(e.target.value)}
+              value={firstName}
+            />
+          </label>
+          <label htmlFor="last-name">
+            Last name<span>*</span>
+            <input
+              type="text"
+              placeholder="Last name"
+              id="last-name"
+              onChange={(e) => setLastName(e.target.value)}
+              value={lastName}
+            />
+          </label>
+        </div>
+        <label htmlFor="dob">
+          Date of Birth<span>*</span>
+          <input
+            type="date"
+            placeholder="DD/MM/YYYY"
+            id="dob"
+            value={formatDateToInputDate(dateOfBirth)}
+            min={formatDateToInputDate(new Date(1900, 0))}
+            max={formatDateToInputDate(MAX_DOB_FOR_ADULT)}
+            onChange={(e) =>
+              setDateOfBirth(formatInputDateToDate(e.target.value))
+            }
+          />
+        </label>
+        <label htmlFor="phone">
+          Telephone<span>*</span>
+          <input
+            type="tel"
+            id="phone"
+            onChange={(e) => setPhone(e.target.value)}
+            value={phone}
+          />
+        </label>
+        <label htmlFor="address">
+          Address<span>*</span>
+          <input
+            type="text"
+            id="address"
+            onChange={(e) => setAddress(e.target.value)}
+            value={address}
+          />
+        </label>
+        <div className="codes">
+          <label htmlFor="city">
+            City<span>*</span>
+            <input
+              type="text"
+              id="city"
+              onChange={(e) => setCity(e.target.value)}
+              value={city}
+            />
+          </label>
+          <label htmlFor="code">
+            Postal Code<span>*</span>
+            <input
+              type="text"
+              id="code"
+              onChange={(e) => setPostalCode(e.target.value)}
+              value={postalCode}
+            />
+          </label>
+        </div>
+        <div className="country">
+          <label htmlFor="country">
+            Country<span>*</span>
+            <input
+              type="text"
+              id="country"
+              onChange={(e) => setCountry(e.target.value)}
+              value={country}
+            />
+          </label>
+          <label htmlFor="state">
+            Province / State<span>*</span>
+            <input
+              type="text"
+              id="state"
+              onChange={(e) => setState(e.target.value)}
+              value={state}
+            />
+          </label>
+        </div>
+      </form>
+    </div>
+  );
+};
---
+++ src/pages/signupformerchant/signup_merchant.component.jsx
@@ -0,0 +1,331 @@
+import "./signup_merchant.styles.scss";
+
+import { useContext, useEffect, useState } from "react";
+import { SignupMerchantFirstPage } from "./signup.merchant_first_page.component";
+import { getConditionalClassNames, getConditionalClassName } from "utils/utils";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { SignupMerchantSecondPage } from "./signup.merchant_second_page.component";
+import { SignupMerchantThirdPage } from "./signup.merchant_third_page.component";
+import { SignupMerchantFourthPage } from "./signup.merchant_fourth_page.component";
+import { Link, useNavigate } from "react-router-dom";
+import { SignupFormContext } from "context/signup-form.context";
+import { RESPONSE_STATES } from "utils/constants";
+import axios from "axios";
+import { Loader } from "components/loader/loader.component";
+
+export const SignupMerchantPage = ({ title }) => {
+  const [page, setPage] = useState(1);
+  const [errorMessage, setErrorMessage] = useState("");
+  const [responseState, setResponseState] = useState(RESPONSE_STATES.none);
+
+  const {
+    email,
+    confirmEmail,
+    password,
+    confirmPassword,
+    firstName,
+    lastName,
+    dateOfBirth,
+    address,
+    city,
+    phone,
+    postalCode,
+    country,
+    state,
+    transactionRange,
+    sourceOfFunds,
+    hearAboutUs,
+    // referredBy,
+    acceptTandC,
+    specifyHeardAboutUs,
+    specifySourceOfFunds,
+  } = useContext(SignupFormContext);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    document.title = title;
+    window.scrollTo(0, 0);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [page]);
+
+  const registerUser = async () => {
+    if (!validateInputs(page)) return;
+    setResponseState(RESPONSE_STATES.loading);
+    let resp;
+    const body = {
+      firstname: firstName,
+      lastname: lastName,
+      email,
+      password,
+      telephone: phone,
+      address,
+      city,
+      state,
+      country,
+      dayOfBirth: dateOfBirth.getDate(),
+      monthOfBirth: dateOfBirth.getMonth() + 1,
+      yearOfBirth: dateOfBirth.getFullYear(),
+      zipCode: postalCode,
+    };
+    try {
+      resp = await axios.post("https://paysprint.ca/api/v1/register", body, {
+        headers: {
+          Authorization:
+            "Bearer base64:JFM+PJaWD/pBypX+NhXudDrAmianZdGYZ41qz4WhXL0=",
+        },
+      });
+
+      if (resp.status === 200) {
+        setResponseState(RESPONSE_STATES.success);
+        navigate("/register");
+        console.log(resp);
+      } else {
+        throw resp.data;
+      }
+    } catch (e) {
+      console.log(e);
+      setResponseState(RESPONSE_STATES.error);
+      if (e?.response?.status === 400) {
+        setErrorMessage(e.response.data?.message);
+      } else {
+        setErrorMessage("Something went wrong. Please try again later.");
+      }
+      console.log(e.response);
+    }
+  };
+
+  const validateInputs = (page) => {
+    if (page === 1) {
+      if (
+        !firstName ||
+        !lastName ||
+        !dateOfBirth ||
+        !address ||
+        !city ||
+        !postalCode ||
+        !country ||
+        !state
+      )
+        return setErrorMessage("All fields must be filled.");
+
+      if (firstName.length < 3 || lastName < 3)
+        return setErrorMessage(
+          "First name and last name must be 3 or more characters"
+        );
+
+      if (dateOfBirth.getFullYear() > new Date().getFullYear() - 18)
+        return setErrorMessage("User must be 18 or above");
+    } else if (page === 2) {
+      if (!email || !confirmEmail || !password || !confirmPassword)
+        return setErrorMessage("All fields must be filled.");
+
+      const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+
+      if (!emailRegex.exec(email))
+        return setErrorMessage("Invalid Email entered.");
+      if (email !== confirmEmail)
+        return setErrorMessage("Email and Confirm Email fields must match.");
+
+      if (password.length < 8)
+        return setErrorMessage("Password cannot be less than 8 characters.");
+
+      if (password !== confirmPassword)
+        return setErrorMessage(
+          "Password and Confirm Password fields must match."
+        );
+    } else if (page === 3) {
+      if (!transactionRange)
+        return setErrorMessage("Select transaction range.");
+
+      if (!sourceOfFunds) return setErrorMessage("Select source of funds");
+
+      if (sourceOfFunds === "others" && !specifySourceOfFunds)
+        return setErrorMessage("Please specify the source of funds");
+    } else if (page === 4) {
+      if (!hearAboutUs) return setErrorMessage("Select how you heard about us");
+
+      if (hearAboutUs === "others" && !specifyHeardAboutUs)
+        return setErrorMessage("You must specify how you heard about us");
+      if (!acceptTandC)
+        return setErrorMessage("Terms and Conditions have not been accepted");
+    }
+    setErrorMessage("");
+    return true;
+  };
+
+  const showSignupPageMerchant = () => {
+    switch (page) {
+      case 1: 
+        return <SignupMerchantFirstPage goToSecondPage={() => pageChange(2)} />;
+      case 2: 
+        return <SignupMerchantSecondPage goToThirdPage={() => pageChange(3)} />;
+      case 3:
+        return <SignupMerchantThirdPage goToFourthPage={() => pageChange(4)} />;
+      case 4:
+        return <SignupMerchantFourthPage />;
+      default:
+        <></>;
+    }
+  };
+
+  const pageChange = (iPage) => {
+    setErrorMessage("");
+    if (iPage >= page) return;
+    setPage(iPage);
+  };
+
+  return (
+    <div className="signup-container">
+      <div className="mobile-progress">
+        <div
+          className={getConditionalClassNames(
+            [page === 1, page > 1],
+            "circle",
+            ["ongoing", "done"]
+          )}
+          onClick={() => pageChange(1)}
+        >
+          {page > 1 ? <FontAwesomeIcon icon={faCheck} /> : "1"}
+        </div>
+        <div
+          className={getConditionalClassName(page > 1, "separator", "active")}
+        />
+        <div
+          className={getConditionalClassNames(
+            [page === 2, page > 2],
+            "circle",
+            ["ongoing", "done"]
+          )}
+          onClick={() => pageChange(2)}
+        >
+          {page > 2 ? <FontAwesomeIcon icon={faCheck} /> : "2"}
+        </div>
+        <div
+          className={getConditionalClassName(page > 2, "separator", "active")}
+        />
+        <div
+          className={getConditionalClassNames(
+            [page === 3, page > 3],
+            "circle",
+            ["ongoing", "done"]
+          )}
+          onClick={() => pageChange(3)}
+        >
+          {page > 3 ? <FontAwesomeIcon icon={faCheck} /> : "3"}
+        </div>
+        <div
+          className={getConditionalClassName(page > 3, "separator", "active")}
+        />
+        <div
+          className={getConditionalClassNames(
+            [page === 4, page > 4],
+            "circle",
+            ["ongoing", "done"]
+          )}
+          onClick={() => pageChange(4)}
+        >
+          {page > 4 ? <FontAwesomeIcon icon={faCheck} /> : "4"}
+        </div>
+      </div>
+      <div className="desktop-progress">
+        <div
+          className={getConditionalClassName(
+            page >= 1,
+            "progress-box",
+            "active"
+          )}
+          onClick={() => pageChange(1)}
+        >
+          <div className="check">
+            <FontAwesomeIcon icon={faCheck} />
+          </div>
+          <div className="text">
+            <h3>Your Details</h3>
+            <em>Please provide your email and necessary information</em>
+          </div>
+        </div>
+        <div
+          className={getConditionalClassName(
+            page >= 2,
+            "progress-box",
+            "active"
+          )}
+          onClick={() => pageChange(2)}
+        >
+          <div className="check">
+            <FontAwesomeIcon icon={faCheck} />
+          </div>
+          <div className="text">
+            <h3>Personal information</h3>
+            <em>Please provide your name and necessary information</em>
+          </div>
+        </div>
+        <div
+          className={getConditionalClassName(
+            page >= 3,
+            "progress-box",
+            "active"
+          )}
+          onClick={() => pageChange(3)}
+        >
+          <div className="check">
+            <FontAwesomeIcon icon={faCheck} />
+          </div>
+          <div className="text">
+            <h3>Account Specifications</h3>
+            <em>Please provide all necessary information</em>
+          </div>
+        </div>
+        <div
+          className={getConditionalClassName(
+            page >= 4,
+            "progress-box",
+            "active"
+          )}
+          onClick={() => pageChange(4)}
+        >
+          <div className="check">
+            <FontAwesomeIcon icon={faCheck} />
+          </div>
+          <div className="text">
+            <h3>Source Attribution</h3>
+            <em> We would like to know how you heard about us </em>
+          </div>
+        </div>
+      </div>
+
+      <section>
+        <main>
+          {showSignupPageMerchant()}
+          {errorMessage && <em className="error">*{errorMessage}</em>}
+          <button type="submit"
+            onClick={() => {
+              if (page === 4) {
+                registerUser();
+              } else if (validateInputs(page)) {
+                setPage(page + 1);
+              }
+            }}
+          >
+            {responseState === RESPONSE_STATES.loading ? (
+              <Loader />
+            ) : page === 4 ? (
+              "Register"
+            ) : (
+              "Proceed"
+            )}
+          </button>
+          <em>
+            Already have an account? <Link to="/login">Sign in</Link>
+          </em>
+        </main>
+      </section>
+    </div>
+  );
+};
