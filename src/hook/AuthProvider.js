dc928109d9d448eafd70b7313d55c5568f9632d0
Praise Ebuka <praise.njoga@gmail.com>
2024-05-17 15:24:24 +0100

    swal msgs added for login screen

--- src/hook/AuthProvider.js
+++ src/hook/AuthProvider.js
@@ -2,6 +2,7 @@ import { useContext, createContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import config from "../config";
 import axios from "axios";
+import Swal from "sweetalert2";
 
 const AuthContext = createContext();
 
@@ -23,12 +24,20 @@ const AuthProvider = ({children}) => {
             }
 
             const result = await axios(apiConfig);
-
+            // console.log(result);
             if (result.data.status === 200) {
                 setUser(result.data.data);
                 setToken(result.data.data.apiToken);
                 localStorage.setItem("authtoken", result.data.data.apiToken);
                 localStorage.setItem("user", JSON.stringify(result.data.data));
+
+                let usersFirstname = result.data.data.name.split(' ');
+                let getName = usersFirstname[1];
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Welcome back',
+                    text: 'Hi ' + getName + ', It\'s nice to have you back. ',
+                  });
                 navigate("/");
                 return;
 
@@ -37,9 +46,19 @@ const AuthProvider = ({children}) => {
 
         } catch (error) {
             if (error.response) {
-                alert(error.response.data.message);
+                // alert(error.response.data.message);
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Please, try again',
+                    text: 'Sorry one of your credentials was not correct',
+                  });
             } else {
-                alert(error.message);
+                // alert(error.message);
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Sorry, Try Again',
+                    text: error.message,
+                  });
             }
         }
     };
