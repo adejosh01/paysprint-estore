b1219e5ee9d10f6c6060119714c726948e3aa701
Praise Ebuka <praise.njoga@gmail.com>
2024-06-03 12:32:23 +0100

    almost done with this side

--- src/pages/myAshopree/myReviews/reviews.page.jsx
+++ src/pages/myAshopree/myReviews/reviews.page.jsx
@@ -1,12 +1,10 @@
 import './reviews.page.scss';
 import { useEffect, useState } from 'react';
 import MyashopreeSidePage from 'components/Myashopree/side.page';
-import product1 from 'assets/images/estore/products/product1.png';
-import product2 from 'assets/images/estore/products/product2.png';
 import google from 'assets/updatedAshopree/google_logo.png';
 import paysprint from 'assets/updatedAshopree/paysprint_backgroundBlack.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleRight, faEnvelopeOpenText, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faAngleRight, faFlag, faSearch } from '@fortawesome/free-solid-svg-icons';
 import { dynamicDisplayEffects } from 'utils/utils';
 
 
@@ -15,7 +13,7 @@ export const MyReviews = ({ title }) => {
     const [value, setValue] = useState(''); 
 
     // Initial active div id is set here
-    const [activeButton, setActiveButton] = useState('allorders');
+    const [activeButton, setActiveButton] = useState('allreviews');
 
     const handleButtonClick = (buttonId) => {
         setActiveButton(buttonId);
@@ -51,142 +49,41 @@ export const MyReviews = ({ title }) => {
 
                         <div className="title-sec"> 
                             <div className='nav-btns'>
-                                <button type='button' className={`button ${activeButton === 'allorders' ? 'active-nav-btn' : ''}`} onClick={() => handleButtonClick('allorders')}> All Orders </button>
-                                <button type='button' className={`button ${activeButton === 'processing' ? 'active-nav-btn' : ''}`} onClick={() => handleButtonClick('processing')}> Processing </button>
-                                <button type='button' className={`button ${activeButton === 'shipped' ? 'active-nav-btn' : ''}`} onClick={() => handleButtonClick('shipped')}> Shipped </button>
-                                <button type='button' className={`button ${activeButton === 'delivered' ? 'active-nav-btn' : ''}`} onClick={() => handleButtonClick('delivered')}> Delivered </button>
-                                <button type='button' className={`button ${activeButton === 'returns' ? 'active-nav-btn' : ''}`} onClick={() => handleButtonClick('returns')}> Returns </button>
+                                <button type='button' className={`button ${activeButton === 'allreviews' ? 'active-nav-btn' : ''}`} onClick={() => handleButtonClick('allreviews')}> All Reviews </button>
+                                <button type='button' className={`button ${activeButton === 'flaggedreviews' ? 'active-nav-btn' : ''}`} onClick={() => handleButtonClick('flaggedreviews')}> Flagged Reviews </button>
                             </div>
                             <form action={`/search?${value}`}>
                                 <div className="searching">
                                     <FontAwesomeIcon icon={faSearch} />
-                                    <input type="text" name="query" placeholder="Search for a product here" value={value} onChange={e => setValue(e.target.value)} />
+                                    <input type="text" name="query" placeholder="Search for a product review here" value={value} onChange={e => setValue(e.target.value)} />
                                 </div>
                             </form>
                         </div>
 
-                        <div className="body-sec active-order" id='allorders'>
+                        <div className="body-sec active-order" id='allreviews'>
                             <div className="title">
-                                <h5> Shipped </h5>
-                                <p> View order details <FontAwesomeIcon icon={faAngleRight} />  </p>
+                                <h5> Comments on Monthly Book Keeping Services by JSC Global Accounting Services. </h5>
+                                <p> View other reviews <FontAwesomeIcon icon={faAngleRight} />  </p>
                             </div>
 
                             <main>
-                                <div>
-                                    <p> Delivery May 22-29 </p>
-                                    <div className="doubleImage">
-                                        <img src={product1} alt="store productImg" />
-                                        <img src={product2} alt="store productImg" />
-                                    </div>
-                                </div>
-                                <div className='minor-btns'>
-                                    <button type='button' className='button active-side'> Track </button>
-                                    <button type='button' className='button ' > Buy this again </button>
-                                    <button type='button' className='button ' > Return/Refund </button>
-                                    <button type='button' className='button ' > Leave a review </button>
-                                    <button type='button' className='button ' > Change address </button>
-                                </div>
+                                
                             </main>
                         </div>
 
-                        <div className="body-sec" id='processing'>
-                            <div className='process-all'>
-                                <h4> You don't have any processing orders <FontAwesomeIcon icon={faEnvelopeOpenText} /> </h4>
+                        <div className="body-sec" id='flaggedreviews'>
+                            <div className='all-flaggedreviews'>
+                                <h4> You don't have any flagged reviews yet <FontAwesomeIcon icon={faFlag} /> </h4>
 
                                 <div>
-                                    <p> Can't find your order? </p>
+                                    <p> Can't find a review here? </p>
                                     <div>
                                         <div>
-                                            <a href="#?"> Try signing in with another account </a>
+                                            <a href="#?"> Try signing in with your normal account </a>
                                             <p> <img src={google} alt="" /> <img style={{ background: '#000' }} src={paysprint} alt="" />  <FontAwesomeIcon icon={faAngleRight} /> </p>
                                         </div>
                                         <div>
-                                            <a href="#?"> Self service to find order  </a>
-                                            <p> <FontAwesomeIcon icon={faAngleRight} /> </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="body-sec" id='shipped'>
-                            <div className="title">
-                                <h5> Shipped </h5>
-                                <p> View order details <FontAwesomeIcon icon={faAngleRight} />  </p>
-                            </div>
-
-                            <main>
-                                <div>
-                                    <p> Delivery May 22-29 </p>
-                                    <div className="doubleImage">
-                                        <img src={product1} alt="store productImg" />
-                                        <img src={product2} alt="store productImg" />
-                                    </div>
-                                </div>
-                                <div className='minor-btns'>
-                                    <button type='button' className='button active-side'> Track </button>
-                                    <button type='button' className='button ' > Buy this again </button>
-                                    <button type='button' className='button ' > Return/Refund </button>
-                                    <button type='button' className='button ' > Leave a review </button>
-                                    <button type='button' className='button ' > Change address </button>
-                                </div>
-                            </main>
-                            
-                            <hr />
-                                <div className='last'>
-                                    <p> 2 items: <span> CA$47.57 </span> </p>
-                                    <p> Order Time: <span> May 16 2024 </span> </p>
-                                    <p> Order ID: <span> PO-037-202327342194552982 </span> </p>
-                                </div>
-                            <hr />
-
-                        </div>
-
-                        <div className="body-sec" id='delivered'>
-                            <div className="title">
-                                <h5> Delivered </h5>
-                                <p> View order details <FontAwesomeIcon icon={faAngleRight} />  </p>
-                            </div>
-
-                            <main>
-                                <div>
-                                    <p> Delivered on May 6, 2024 </p>
-                                    <div className="doubleImage">
-                                        <img src={product1} alt="store productImg" />
-                                        <img src={product2} alt="store productImg" />
-                                    </div>
-                                </div>
-                                <div className='minor-btns'>
-                                    <button type='button' className='button active-side'> Track </button>
-                                    <button type='button' className='button ' > Leave a review </button>
-                                    <button type='button' className='button ' > Return/Refund </button>
-                                    <button type='button' className='button ' > Buy this again </button>
-                                </div>
-                            </main>
-                            
-                            <hr />
-                                <div className='last'>
-                                    <p> 2 items: <span> CA$54.98 </span> </p>
-                                    <p> Order Time: <span> April 30 2024 </span> </p>
-                                    <p> Order ID: <span> PO-037-202327342194552982 </span> </p>
-                                </div>
-                            <hr />
-
-                        </div>
-
-                        <div className="body-sec" id='returns'>
-                            <div className='process-all'>
-                                <h4> You don't have any processing orders <FontAwesomeIcon icon={faEnvelopeOpenText} /> </h4>
-
-                                <div>
-                                    <p> Can't find your order? </p>
-                                    <div>
-                                        <div>
-                                            <a href="#?"> Try signing in with another account </a>
-                                            <p> <img src={google} alt="" /> <img style={{ background: '#000' }} src={paysprint} alt="" />  <FontAwesomeIcon icon={faAngleRight} /> </p>
-                                        </div>
-                                        <div>
-                                            <a href="#?"> Self service to find order  </a>
+                                            <a href="#?"> Self service to find review  </a>
                                             <p> <FontAwesomeIcon icon={faAngleRight} /> </p>
                                         </div>
                                     </div>
