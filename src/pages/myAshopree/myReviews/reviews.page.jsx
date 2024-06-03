adcfb13051f5b9a822f78afcce27c1e0b5c8b436
Praise Ebuka <praise.njoga@gmail.com>
2024-06-03 16:09:18 +0100

    background changed

--- src/pages/myAshopree/myReviews/reviews.page.jsx
+++ src/pages/myAshopree/myReviews/reviews.page.jsx
@@ -67,7 +67,94 @@ export const MyReviews = ({ title }) => {
                             </div>
 
                             <main>
-                                
+                                <div className="one-thread">
+                                    <div className='user-comment'>
+                                        <div className="avatar">
+                                            <img src="https://res.cloudinary.com/dysgwap7p/image/upload/v1665052608/Logikeep/avatar-image_oqym5e.png" alt="userIcon" />
+                                        </div>
+
+                                        <div className="comment-box">
+                                            <div className="comment-head">
+                                                <h6> Somoye Munirat <span> 10/06/2022, 09:46:44 AM </span> </h6>
+                                                <hr />
+                                                <p className="comment-content"> Thanks for shopping </p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div className="my-reply">
+                                        <div className="avatar">
+                                            <img src="https://paysprint.ca/shopstore/JSC_Global_Accounting_Services/myproduct/1231891620_1697759965.jpeg" alt="userIcon" />
+                                        </div>
+
+                                        <div className="reply-box">
+                                            <div className="comment-head">
+                                                <h6> Monthly Book Keeping Services <span> 10/06/2022, 09:48:44 AM </span> </h6>
+                                                <hr />
+                                                <p className="comment-content"> Thanks you </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="one-thread">
+                                    <div className='user-comment'>
+                                        <div className="avatar">
+                                            <img src="https://res.cloudinary.com/dysgwap7p/image/upload/v1665052608/Logikeep/avatar-image_oqym5e.png" alt="userIcon" />
+                                        </div>
+
+                                        <div className="comment-box">
+                                            <div className="comment-head">
+                                                <h6> Abimbola Portable <span> 10/6/2022, 9:54:41 AM </span> </h6>
+                                                <hr />
+                                                <p className="comment-content"> I love your prompt service </p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div className="my-reply">
+                                        <div className="avatar">
+                                            <img src="https://paysprint.ca/shopstore/JSC_Global_Accounting_Services/myproduct/1231891620_1697759965.jpeg" alt="userIcon" />
+                                        </div>
+
+                                        <div className="reply-box">
+                                            <div className="comment-head">
+                                                <h6> Monthly Book Keeping Services <span> 10/06/2022, 05:57:01 AM </span> </h6>
+                                                <hr />
+                                                <p className="comment-content"> Thanks my friend </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="one-thread">
+                                    <div className='user-comment'>
+                                        <div className="avatar">
+                                            <img src="https://res.cloudinary.com/dysgwap7p/image/upload/v1665052608/Logikeep/avatar-image_oqym5e.png" alt="userIcon" />
+                                        </div>
+
+                                        <div className="comment-box">
+                                            <div className="comment-head">
+                                                <h6> Somoye Munirat <span> 10/6/2022, 11:29:26 AM </span> </h6>
+                                                <hr />
+                                                <p className="comment-content"> Good Service </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="one-thread">
+                                    <div className='user-comment'>
+                                        <div className="avatar">
+                                            <img src="https://res.cloudinary.com/dysgwap7p/image/upload/v1665052608/Logikeep/avatar-image_oqym5e.png" alt="userIcon" />
+                                        </div>
+
+                                        <div className="comment-box">
+                                            <div className="comment-head">
+                                                <h6> Moses <span> 10/24/2022, 10:56:44 AM </span> </h6>
+                                                <hr />
+                                                <p className="comment-content"> I love your services </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </main>
                         </div>
 
