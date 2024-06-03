10453dccc657be9848321036a9410844fe42c868
Praise Ebuka <praise.njoga@gmail.com>
2024-06-03 11:41:10 +0100

    in preparation for review page

--- src/App.js
+++ src/App.js
@@ -37,6 +37,7 @@ import { StoreLocation } from "pages/myAshopree/storeLocation/storeLocation.page
 import { GetStarted } from "pages/getstarted/getstarted";
 import { CommunityPost } from "pages/community/commuinity-post";
 import { TrackOrders } from "pages/myAshopree/trackOrders/trackOrders.page";
+import { MyReviews } from "pages/myAshopree/myReviews/reviews.page";
 
 function App() {
 
@@ -64,6 +65,7 @@ function App() {
               <Route path="/myashopree" exact element={<Myashopree title="aShopree | My aShopree " />} />
               <Route path="/store-location" exact element={<StoreLocation title="aShopree | My Shoppings " />} />
               <Route path="/track-orders" exact element={<TrackOrders title="aShopree | Track Orders " />} />
+              <Route path="/reviews" exact element={<MyReviews title="aShopree | My Reviews " />} />
             </Route>
 
             
