f1a9e536e4c79d4278e97c12cbd1b2271413f92f
Praise Ebuka <praise.njoga@gmail.com>
2024-06-08 14:16:10 +0100

    promo mod

--- src/pages/myAshopree/purchaseHistory/purchasehistory.jsx
+++ src/pages/myAshopree/purchaseHistory/purchasehistory.jsx
@@ -1,16 +1,15 @@
-import './myashopree.styles.scss';
+import './purchasehistory.styles.scss';
 import { useEffect } from 'react';
 import MyashopreeSidePage from 'components/Myashopree/side.page';
 import exchange_icon from 'assets/svg/reward_page/exchange.png';
 import cashback from 'assets/svg/reward_page/cash_back.png';
 import review from 'assets/svg/reward_page/review_points.png';
 import ref from 'assets/svg/reward_page/referral_points.png';
-import ProgressBar from 'components/loader/progress.bar.component';
 import AshopreePromos from 'components/Myashopree/promos';
 
 
 
-export const Myashopree = ({ title }) => {
+export const MyPurchaseHistory = ({ title }) => {
 
     useEffect(() => {
       document.title = title;
@@ -21,7 +20,7 @@ export const Myashopree = ({ title }) => {
     return (
         <div className="estore-container">
 
-            <section className="myashopree">
+            <section className="mypurchase-history">
                 
                 <section className="all-items">
                     <MyashopreeSidePage />
@@ -30,7 +29,7 @@ export const Myashopree = ({ title }) => {
                         <div className="main-element">
                             <div className='first'>
                                 <div className='head'>
-                                    <h4> Reward Summary </h4>
+                                    <h4> Purchase History </h4>
                                     <div>
                                         <img src={exchange_icon} alt="" />
                                         <p> Point exchange </p>
@@ -61,45 +60,6 @@ export const Myashopree = ({ title }) => {
                                             <p> 0.5% from yesterday </p>
                                         </div>
                                     </div>
-                                </div>
-                            </div>
-                            <div className='second'>
-                                <h4> Tasks </h4>
-                                <div className="table">
-                                    <table>
-                                        <thead>
-                                            <th> # </th>
-                                            <th> Name </th>
-                                            <th> Progress </th>
-                                            <th> Status </th>
-                                        </thead> <hr />
-                                        <tbody>
-                                            <tr>
-                                                <td> 01 </td>
-                                                <td> Refer 5 people </td>
-                                                <td style={{ width: '25%' }}> <ProgressBar width={75} /> </td>
-                                                <td> <button type="button" > Redeem </button> </td>
-                                            </tr> <hr />
-                                            <tr>
-                                                <td> 02 </td>
-                                                <td> Post twice on facebook </td>
-                                                <td style={{ width: '25%' }}> <ProgressBar width={60} /> </td>
-                                                <td> <button type="button" > Redeem </button> </td>
-                                            </tr> <hr />
-                                            <tr>
-                                                <td> 03 </td>
-                                                <td> Comment on 4 posts </td>
-                                                <td style={{ width: '25%' }}> <ProgressBar width={50} /> </td>
-                                                <td> <button type="button" > Redeem </button> </td>
-                                            </tr> <hr />
-                                            <tr>
-                                                <td> 04 </td>
-                                                <td> Share link 3 times </td>
-                                                <td style={{ width: '25%' }}> <ProgressBar width={30} /> </td>
-                                                <td> <button type="button" > Redeem </button> </td>
-                                            </tr> <hr />
-                                        </tbody>
-                                    </table>
                                 </div>
                             </div>
                         </div>
