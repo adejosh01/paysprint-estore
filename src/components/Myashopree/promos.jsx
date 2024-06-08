1cfaed8d1a91d63c33e7b4b8d7be0e12ad781149
Praise Ebuka <praise.njoga@gmail.com>
2024-06-08 14:12:21 +0100

    promo component :)

---
+++ src/components/Myashopree/promos.jsx
@@ -0,0 +1,64 @@
+
+
+const AshopreePromos = () => {
+
+    return (
+        <div className="promos">
+            <h4> Promos </h4>
+
+            <div className='parent'>
+                <div>
+                    <div> <p> 01 </p> <p> Freebies </p> </div>
+                    <button> Get </button>
+                </div>
+                <div>
+                    <div> <p> 02 </p> <p> Free shipping </p> </div>
+                    <button> Get </button>
+                </div>
+                <div>
+                    <div> <p> 03 </p> <p> Order discount </p> </div>
+                    <button> Get </button>
+                </div>
+                <div>
+                    <div> <p> 01 </p> <p> Freebies </p> </div>
+                    <button> Get </button>
+                </div>
+                <div>
+                    <div> <p> 02 </p> <p> Free shipping </p> </div>
+                    <button> Get </button>
+                </div>
+                <div>
+                    <div> <p> 03 </p> <p> Order discount </p> </div>
+                    <button> Get </button>
+                </div>
+                <div>
+                    <div> <p> 01 </p> <p> Freebies </p> </div>
+                    <button> Get </button>
+                </div>
+                <div>
+                    <div> <p> 02 </p> <p> Free shipping </p> </div>
+                    <button> Get </button>
+                </div>
+                <div>
+                    <div> <p> 03 </p> <p> Order discount </p> </div>
+                    <button> Get </button>
+                </div>
+                <div>
+                    <div> <p> 01 </p> <p> Freebies </p> </div>
+                    <button> Get </button>
+                </div>
+                <div>
+                    <div> <p> 02 </p> <p> Free shipping </p> </div>
+                    <button> Get </button>
+                </div>
+                <div>
+                    <div> <p> 03 </p> <p> Order discount </p> </div>
+                    <button> Get </button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+
+export default AshopreePromos;
