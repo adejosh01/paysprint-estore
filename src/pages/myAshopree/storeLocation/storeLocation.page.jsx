0e715aed8cef28a4e5e4b53b15340ace6a835cbf
Praise Ebuka <praise.njoga@gmail.com>
2024-06-21 17:04:20 +0100

    alright.....cool

--- src/pages/myAshopree/storeLocation/storeLocation.page.jsx
+++ src/pages/myAshopree/storeLocation/storeLocation.page.jsx
@@ -91,7 +91,7 @@ export const StoreLocation = ({ title }) => {
                             </main>
                         )
                     }
-                        
+                    
                 </section>
 
             </section>
